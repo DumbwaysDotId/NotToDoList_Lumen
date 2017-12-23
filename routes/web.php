3c3a32382015d276150a71a06d5733599ff37332
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-12-23 16:00:25 +0700

    17.lumen routing

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('todos', function ()    {
+        return "Todos";
+    });
 });
